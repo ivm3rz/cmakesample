080302eb3a1a91515acd93e37ac7c2ba83ba6345
Merzlyakov Igor <Igor.Merzlyakov@infotecs.ru>
2024-03-11 16:07:04 +0300

    fix indent

--- main.cpp
+++ main.cpp
@@ -36,14 +36,14 @@
 
 int main( int argc, char* argv[] )
 {
-    // SailfishApp::main() will display "qml/cmakesample.qml", if you need more
-    // control over initialization, you can use:
-    //
-    //   - SailfishApp::application(int, char *[]) to get the QGuiApplication *
-    //   - SailfishApp::createView() to get a new QQuickView * instance
-    //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
-    //
-    // To display the view, call "show()" (will show fullscreen on device).
+     // SailfishApp::main() will display "qml/cmakesample.qml", if you need more
+     // control over initialization, you can use:
+     //
+     //   - SailfishApp::application(int, char *[]) to get the QGuiApplication *
+     //   - SailfishApp::createView() to get a new QQuickView * instance
+     //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
+     //
+     // To display the view, call "show()" (will show fullscreen on device).
 
      return Aurora::Application::main( argc, argv );
 }
